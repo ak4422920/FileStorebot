d50eb62abc880f0de3de4a394d0a45678d269ddd
ak4422920 <ak4422920@gmail.com>
2025-07-06 11:24:32 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -146,7 +146,7 @@ async def start_command(client: Client, message: Message):
                     else None
                 )
                 keyboard = InlineKeyboardMarkup(
-                    [[InlineKeyboardButton("ɢᴇᴛ ғɪʟᴇ ᴀɢᴀɪɴ!", url=reload_url)]]
+                    [[InlineKeyboardButton("ɢᴇᴛ ғɪʟᴇ ᴀɢᴀɪɴ!", url=https://t.me/cineoriginals/59)]]
                 ) if reload_url else None
 
                 await notification_msg.edit(
