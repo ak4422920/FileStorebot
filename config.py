d5c4fd0c27e1d235e45a114ea7c9bc12605dc90f
ak4422920 <ak4422920@gmail.com>
2026-07-11 09:06:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003774344278")) #Your db channel Id
-OWNER = os.environ.get("OWNER", "NullZoro") # Owner username without @
+OWNER = os.environ.get("OWNER", "PurelySinVipBot") # Owner username without @
 OWNER_ID = int(os.environ.get("OWNER_ID", "6522435665")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
